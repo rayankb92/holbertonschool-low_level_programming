97ce8b6a3b3a0d885f040d6879a3d2115f521908
rayankb92 <ferradi.r@gmail.com>
2022-06-07 20:21:12 +0200

    ex 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 0 to 10 with putchar
+ *Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
