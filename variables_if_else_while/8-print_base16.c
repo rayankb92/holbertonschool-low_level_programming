58d7a72814fc2cb435ea5076ebaf77e970575c4f
rayankb92 <ferradi.r@gmail.com>
2022-06-07 20:40:59 +0200

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print base 16
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+	int s;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (s = 97; s <= 102; s++)
+	{
+		putchar(S);
+	}
+	putchar('\n');
+	return (0);
+}
