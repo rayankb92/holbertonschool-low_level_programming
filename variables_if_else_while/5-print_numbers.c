ad28353c351db92b23829c85101f7b109e641363
rayankb92 <ferradi.r@gmail.com>
2022-06-07 20:14:57 +0200

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers from 0 to 9
+ *Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
