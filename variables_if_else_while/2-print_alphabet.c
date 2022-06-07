88b40ba9ff22f59bb59fb3812565a5bb0e512943
rayankb92 <ferradi.r@gmail.com>
2022-06-07 14:31:33 +0200

    putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print alphabet
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+	putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
