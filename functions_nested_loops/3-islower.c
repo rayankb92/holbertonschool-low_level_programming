bb252909c20e8621fa4b90edf8f8debd32aba46c
rayankb92 <ferradi.r@gmail.com>
2022-06-08 14:11:08 +0200

    update shorter without 'islower'

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -7,13 +7,5 @@
 
 int _islower(int c)
 {
-	
-	if (islower(c))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (c >= 'a' && c <= 'z');
 }
--- functions_nested_loops/3-main.c
+++ functions_nested_loops/3-main.c
@@ -13,7 +13,7 @@ int main(void)
     _putchar(r + '0');
     r = _islower('o');
     _putchar(r + '0');
-    r = _islower(108);
+    r = _islower(121);
     _putchar(r + '0');
     _putchar('\n');
     return (0);
