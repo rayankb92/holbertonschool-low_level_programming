27ad48960bcb6327d0940a7e73f25bf4319dc951
rayankb92 <ferradi.r@gmail.com>
2022-06-08 13:21:45 +0200

    ex 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print 10x alphabet
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+
+	a = 'a';
+
+	while (a < z)
+	{
+		_putchar(a);
+		a++;
+	{
+	_putchar('\n');
+	
+}
