d0ce4509c37d8082f0e1120510be6c3a34100878
rayankb92 <ferradi.r@gmail.com>
2022-06-09 11:27:22 +0200

    update

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,16 +9,19 @@
 int print_last_digit(int n)
 {
 	int a;
+	int min;
 
-	if (n < 0)
+	min = -2147483647;
+	if (n == min)
+	{
+		n--;
+		n = -n;
+	}
+	else if (n < 0)
 	{
 		n = -n;
-		a = n % 10;
 	}
-	else
-	{
-		a = n % 10;
-	}
+	a = n % 10;
 	_putchar(a + '0');
 	return (a);
 }
