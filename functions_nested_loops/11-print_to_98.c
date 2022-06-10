b9b789b44b9af150b4fc45b73f28888551fb1775
rayankb92 <ferradi.r@gmail.com>
2022-06-10 14:04:25 +0200

    add task 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * print_to_98 - print any number to 98
+ * @n: first arg
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		if (i < 0)
+		{
+			i = -i;
+			_putchar('-');
+			if (i > 9 && i != 0)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+				_putchar(i + '0');
+			}
+			i = -i;
+		}
+		else if (i < 10)
+		{
+			_putchar(i + '0');
+		}
+		else
+		{
+		_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
+		}
+		if (i != 98)
+		{
+		_putchar(',');
+		_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
