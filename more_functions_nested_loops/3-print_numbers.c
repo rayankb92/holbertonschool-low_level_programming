5e3e286ecfe56b2ad4c5ba60287f66429bf03e51
rayankb92 <ferradi.r@gmail.com>
2022-06-13 17:46:49 +0200

    add task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - explicit
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
